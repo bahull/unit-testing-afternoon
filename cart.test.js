fb09cb900747d2fd3896d2c1dcb9d77467a45c1f
Bryce Hull <bahull@live.com>
2017-12-14 12:23:12 -0600

    On to step 3

---
+++ cart.test.js
@@ -0,0 +1,6 @@
+const { addToCart, removeFromCart, checkout } = require('./cart');
+const dataCars = require('./data/cars');
+
+describe('Cart Properties: ', () => {});
+
+describe('Cart Methods: ', () => {});
